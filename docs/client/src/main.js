6d9ea0320f2129fa1949c72f8b0c250c4b8be94e
City-Hall <qwert123456654321@gmail.com>
2021-03-07 14:38:12 -0500

    Full

---
+++ docs/client/src/main.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import FriendsShort from "./components/FriendsShort";
+
+import 'bulma';
+import 'buefy/dist/buefy.css';
+import '@fortawesome/fontawesome-free/css/all.css';
+import './sass/main.scss';
+
+Vue.component('FriendsShort', FriendsShort);
+Vue.config.productionTip = false
+
+new Vue({
+  router,
+  render: h => h(App)
+}).$mount('#app')

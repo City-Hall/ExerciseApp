c39bb8cb6affc9b9734903e4fab757db3df413d7
Andrew Calvarese <78175214+City-Hall@users.noreply.github.com>
2021-03-09 10:59:35 -0500

    class update

--- client/src/models/Session.js
+++ client/src/models/Session.js
@@ -1,4 +1,13 @@
-export default {
+const Session = {
     user: null,
     message: [] // To be used later
+}
+export default Session;
+
+export function Login(){
+    Session.user = { name: "Andrew", handle: '@Andrew' }
+}
+
+export function Logout(){
+    Session.user = null
 }

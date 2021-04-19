edaa12b802f883dba7bb9336f6c709febaec0f58
Andrew Calvarese <78175214+City-Hall@users.noreply.github.com>
2021-04-19 11:15:42 -0400

    env and fetch

--- client/src/views/Home.vue
+++ client/src/views/Home.vue
@@ -1,8 +1,7 @@
-  
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png" id="home-img">
-    <h1> Hi this is an exercise app</h1>
+    <h1> Hi this is an Excersise App</h1>
     
     
   </div>
@@ -10,6 +9,11 @@
 
 <script>
 // @ is an alias to /src
+import { api } from "../models/myFetch";
+(async ()=>{
+  const actualItem = await api("posts");
+  console.log({actualItem});
+})()
 export default {
   name: 'Home',
   components: {
--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const express = require('express');
 const dotenv = require('dotenv');
+const cors = require('cors');
 dotenv.config();
 
 require('dotenv').config();
@@ -16,30 +17,32 @@ const port = process.env.PORT || 3000;
 app
     .use(express.json())
     .use(express.static('./docs'))
+    .use(cors())
 
     .use(async (req, res, next)=>{ 
         
-        const token = req.headers.authorization?.split(' ')[1];
-        req.user = token && await usersModel.FromJWT(token);
-        next();
-    }) 
+      const token = req.headers.authorization?.split(' ')[1];
+      req.user = token && await usersModel.FromJWT(token);
+      req.user = { isAdmin: true }
+      next();
+  }) 
 
-    .use('/users', usersCtrl)
-    .use('/posts', LoginRequired, postsCtrl)
+  .use('/users', usersCtrl)
+  .use('/posts', LoginRequired, postsCtrl)
 
 
-    // All the way at the end of the pipeline. Return instead of not found
-    .get('*', (req, res) => {
-        res.sendFile( path.join(__dirname, '../docs/index.html' ) );
-    })
+  // All the way at the end of the pipeline. Return instead of not found
+  .get('*', (req, res) => {
+      res.sendFile( path.join(__dirname, '../docs/index.html' ) );
+  })
 
-    .use((error, req, res, next)=>{
-      console.error(error);
+  .use((error, req, res, next)=>{
+    console.error(error);
 
-        res.status(error.code || 500 );
-        res.send( { msg: error.msg });
-    })
+      res.status(error.code || 500 );
+      res.send( { msg: error.msg });
+  })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
+console.log(`Example app listening at http://localhost:${port}`)
 })
